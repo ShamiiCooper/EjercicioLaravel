6ab4405113e0369d6e7ffad8e35d9cf737bc0a66
YamiMarucci <ymarucci@gmail.como.com>
2018-07-06 13:56:59 -0300

    Created migrations

---
+++ database/migrations/2018_07_06_154125_create_clients_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreateClientsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('clients', function (Blueprint $table) {
+            $table->engine = 'InnoDB';
+            $table->increments('id');
+            $table->string('nombre');
+            $table->string('direccion');
+            $table->date('fecha_alta');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('clients');
+    }
+}
